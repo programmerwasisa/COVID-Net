3b62af233faacd46b8d72d96f91f0d50749da6cb
programmerwasisa <programmerwasisa@gmail.com>
2020-10-07 22:39:23 +0300

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,41 +1,98 @@
+# USAGE
+
+
+# import the necessary packages
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.applications import InceptionV3
+from tensorflow.keras.applications import Xception # TensorFlow ONLY
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.applications import VGG19
+from tensorflow.keras.applications import imagenet_utils
+from tensorflow.keras.applications.inception_v3 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
 import numpy as np
-import tensorflow as tf
-import os, argparse
+import argparse
 import cv2
 
-from data import process_image_file
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to the input image")
+ap.add_argument("-model", "--model", type=str, default="vgg16",
+	help="name of pre-trained network to use")
+args = vars(ap.parse_args())
 
-parser = argparse.ArgumentParser(description='COVID-Net Inference')
-parser.add_argument('--weightspath', default='models/COVIDNet-CXR3-S', type=str, help='Path to output folder')
-parser.add_argument('--metaname', default='model.meta', type=str, help='Name of ckpt meta file')
-parser.add_argument('--ckptname', default='model-1014', type=str, help='Name of model ckpts')
-parser.add_argument('--imagepath', default='assets/ex-covid.jpeg', type=str, help='Full path to image to be inferenced')
-parser.add_argument('--in_tensorname', default='input_1:0', type=str, help='Name of input tensor to graph')
-parser.add_argument('--out_tensorname', default='norm_dense_1/Softmax:0', type=str, help='Name of output tensor from graph')
-parser.add_argument('--input_size', default=480, type=int, help='Size of input (ex: if 480x480, --input_size 480)')
-parser.add_argument('--top_percent', default=0.08, type=float, help='Percent top crop from top of image')
+# define a dictionary that maps model names to their classes
+# inside Keras
+MODELS = {
+	"vgg16": VGG16,
+	"vgg19": VGG19,
+	"inception": InceptionV3,
+	"xception": Xception, # TensorFlow ONLY
+	"resnet": ResNet50
+}
 
-args = parser.parse_args()
+# esnure a valid model name was supplied via command line argument
+if args["model"] not in MODELS.keys():
+	raise AssertionError("The --model command line argument should "
+		"be a key in the `MODELS` dictionary")
 
-mapping = {'normal': 0, 'pneumonia': 1, 'COVID-19': 2}
-inv_mapping = {0: 'normal', 1: 'pneumonia', 2: 'COVID-19'}
+# initialize the input image shape (224x224 pixels) along with
+# the pre-processing function (this might need to be changed
+# based on which model we use to classify our image)
+inputShape = (224, 224)
+preprocess = imagenet_utils.preprocess_input
 
-sess = tf.Session()
-tf.get_default_graph()
-saver = tf.train.import_meta_graph(os.path.join(args.weightspath, args.metaname))
-saver.restore(sess, os.path.join(args.weightspath, args.ckptname))
+# if we are using the InceptionV3 or Xception networks, then we
+# need to set the input shape to (299x299) [rather than (224x224)]
+# and use a different image pre-processing function
+if args["model"] in ("inception", "xception"):
+	inputShape = (299, 299)
+	preprocess = preprocess_input
 
-graph = tf.get_default_graph()
+# load our the network weights from disk (NOTE: if this is the
+# first time you are running this script for a given network, the
+# weights will need to be downloaded first -- depending on which
+# network you are using, the weights can be 90-575MB, so be
+# patient; the weights will be cached and subsequent runs of this
+# script will be *much* faster)
+print("[INFO] loading {}...".format(args["model"]))
+Network = MODELS[args["model"]]
+model = Network(weights="imagenet")
 
-image_tensor = graph.get_tensor_by_name(args.in_tensorname)
-pred_tensor = graph.get_tensor_by_name(args.out_tensorname)
+# load the input image using the Keras helper utility while ensuring
+# the image is resized to `inputShape`, the required input dimensions
+# for the ImageNet pre-trained network
+print("[INFO] loading and pre-processing image...")
+image = load_img(args["image"], target_size=inputShape)
+image = img_to_array(image)
 
-x = process_image_file(args.imagepath, args.top_percent, args.input_size)
-x = x.astype('float32') / 255.0
-pred = sess.run(pred_tensor, feed_dict={image_tensor: np.expand_dims(x, axis=0)})
+# our input image is now represented as a NumPy array of shape
+# (inputShape[0], inputShape[1], 3) however we need to expand the
+# dimension by making the shape (1, inputShape[0], inputShape[1], 3)
+# so we can pass it through the network
+image = np.expand_dims(image, axis=0)
 
-print('Prediction: {}'.format(inv_mapping[pred.argmax(axis=1)[0]]))
-print('Confidence')
-print('Normal: {:.3f}, Pneumonia: {:.3f}, COVID-19: {:.3f}'.format(pred[0][0], pred[0][1], pred[0][2]))
-print('**DISCLAIMER**')
-print('Do not use this prediction for self-diagnosis. You should check with your local authorities for the latest advice on seeking medical assistance.')
+# pre-process the image using the appropriate function based on the
+# model that has been loaded (i.e., mean subtraction, scaling, etc.)
+image = preprocess(image)
+
+# classify the image
+print("[INFO] classifying image with '{}'...".format(args["model"]))
+preds = model.predict(image)
+P = imagenet_utils.decode_predictions(preds)
+
+# loop over the predictions and display the rank-5 predictions +
+# probabilities to our terminal
+for (i, (imagenetID, label, prob)) in enumerate(P[0]):
+	print("{}. {}: {:.2f}%".format(i + 1, label, prob * 100))
+
+# load the image via OpenCV, draw the top prediction on the image,
+# and display the image to our screen
+orig = cv2.imread(args["image"])
+(imagenetID, label, prob) = P[0][0]
+cv2.putText(orig, "Label: {}, {:.2f}%".format(label, prob * 100),
+	(10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
+cv2.imshow("Classification", orig)
+cv2.waitKey(0)
